e981543e6d1c87a56d9da5955c894cbba138dd96
Chawlucha-hub <liam.ahlberg2008@gmail.com>
2026-08-31 12:54:31 +0200

    bacik movment

--- Assets/skrips/player/movment.cs
+++ Assets/skrips/player/movment.cs
@@ -2,15 +2,27 @@ using UnityEngine;
 
 public class movment : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private PlayerInputActions inputActions;
+    [SerializeField] private float moveSpeed = 5f;
+
+    private void Awake()
     {
-        
+        inputActions = new PlayerInputActions();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        inputActions.Enable();
+    }
+
+    private void OnDisable()
+    {
+        inputActions.Disable();
+    }
+
+    private void Update()
+    {
+        Vector2 moveInput = inputActions.Player.Move.ReadValue<Vector2>();
+        transform.Translate(new Vector3(moveInput.x, moveInput.y, 0) * moveSpeed * Time.deltaTime);
     }
 }
