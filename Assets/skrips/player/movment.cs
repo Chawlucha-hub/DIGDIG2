253c15b06aaf1d81eefd4ccb52dd208f5a9e2fb9
Chawlucha-hub <liam.ahlberg2008@gmail.com>
2026-09-07 13:04:10 +0200

    colideruptete

--- Assets/skrips/player/movment.cs
+++ Assets/skrips/player/movment.cs
@@ -1,4 +1,3 @@
-using UnityEditor.Rendering;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -14,11 +13,15 @@ public class movment : MonoBehaviour
     [SerializeField] private GameObject healthBar;
     private Vector3 healthBarPos;
 
+    // Fysik
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+
     private void Awake()
     {
         inputActions = new PlayerInputActions();
-       healthBarPos = healthBar.transform.position;
-
+        healthBarPos = healthBar.transform.position;
+        rb = GetComponent<Rigidbody2D>();
     }
 
     private void OnEnable()
@@ -33,9 +36,8 @@ public class movment : MonoBehaviour
 
     private void Update()
     {
-        // WADS movent
-        Vector2 moveInput = inputActions.Player.Move.ReadValue<Vector2>();
-        transform.Translate(new Vector3(moveInput.x, moveInput.y, 0) * moveSpeed * Time.deltaTime);
+        // WADS movent (Sparar värdet för FixedUpdate)
+        moveInput = inputActions.Player.Move.ReadValue<Vector2>();
 
         // musposition så man kan tita åt alla holl
         Vector2 mouseScreenPos = Mouse.current.position.ReadValue();
@@ -53,7 +55,7 @@ public class movment : MonoBehaviour
         transform.rotation = Quaternion.Euler(0f, 0f, angle - 90);
 
 
-        camara.transform.position = new Vector3 (gameObject.transform.position.x,gameObject.transform.position.y,-10f);
+        camara.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, -10f);
         // gör att kamramran inte snrar
         camara.transform.rotation = Quaternion.identity;
 
@@ -61,7 +63,17 @@ public class movment : MonoBehaviour
         healthBar.TryGetComponent<RectTransform>(out RectTransform rectTransform);
 
         rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, health * 5f);
-        healthBar.transform.position = healthBarPos + new Vector3( -250f + health * 2.5f, 0f, 0f);
+        healthBar.transform.position = healthBarPos + new Vector3(-250f + health * 2.5f, 0f, 0f);
+    }
 
+    private void FixedUpdate()
+    {
+        // Omvandlar WASD-inputen baserat på vart spelaren pekar!
+       
+        Vector2 moveDirection = (transform.up * moveInput.y) + (transform.right * moveInput.x);
+
+        // Sätter fysikhastigheten i spelarens lokala riktning
+        rb.linearVelocity = moveDirection * moveSpeed;
     }
 }
+
