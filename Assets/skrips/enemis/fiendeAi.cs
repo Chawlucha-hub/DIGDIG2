33a35d496e87e204a9a0db28d8c6cb47f3578045
Chawlucha-hub <liam.ahlberg2008@gmail.com>
2026-09-14 12:32:05 +0200

    ai updeate

--- Assets/skrips/enemis/fiendeAi.cs
+++ Assets/skrips/enemis/fiendeAi.cs
@@ -3,25 +3,39 @@ using UnityEngine;
 public class FiendeAi : MonoBehaviour
 {
     [SerializeField] private float circleRadius = 2f;
-   
+    [SerializeField] private GameObject player;
+    [SerializeField] private LayerMask Player;
+    [SerializeField] private LayerMask notEnemy;
 
 
 
     void Update()
     {
+        // hitar spelarens relativa riktning
+        Vector2 playerDir = (player.transform.position - gameObject.transform.position).normalized;
 
         // Kollar allt inom cirkeln runt fienden
-        Collider2D hit = Physics2D.OverlapCircle(transform.position, circleRadius);
+        Collider2D hit = Physics2D.OverlapCircle(transform.position, circleRadius, Player);
 
         if (hit != null)
         {
-            if (hit.name == "Player")
+            if ( hit.name == "Player")
             {
-                // RaycastHit2D hit2D = Physics2D.GetContactColliders();
-            }
 
-            Debug.Log("Träffade: " + hit.name);
+                float distanceToPlayer = Vector2.Distance(transform.position, player.transform.position);
+
+                RaycastHit2D hit2D = Physics2D.Raycast(gameObject.transform.position, playerDir, distanceToPlayer, notEnemy);
+
+                if (hit2D != null && hit2D.collider.name == "Player")
+                {
+                    
+                }
+                
+            }
         }
+            
+
+        
     }
 
     // Ritar BARA EN cirkel i Scene-vyn
